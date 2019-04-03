15b93ff333550b918f54fae18f4547000d301a24
PARKYOONJUNG <201602001@o.cnu.ac.kr>
2019-04-03 22:29:58 +0900

    minigo
    minigo

---
+++ 2주차/test.go
@@ -0,0 +1,41 @@
+
+var a int
+var z int = 10
+var ans [15] int
+func main() {
+	if z < 0 {
+		return
+	}
+   a = 100
+   sum(1,2)
+   test1(z)
+   test2()
+}
+
+func test2(){
+	fmt.Println(a)
+}
+ 
+func sum(a int,b int) int {
+	var t int
+	if a < 0 {
+		a = 2 * a
+	} else {
+		t = a + b
+	}
+	return t
+}
+
+func test1(x) (int, int){
+	var st int = 1
+	var te[5] int
+	for i = 0; i < 15; i++ {
+		ans[i] = i
+	}
+	
+	for te[0] < 5 {
+		--st
+	}
+	return st,x
+}
+
